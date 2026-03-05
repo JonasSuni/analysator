8d0d118e48982d24db3bd2b8e863a201770cd05d
JonasSuni <jonas.suni@helsinki.fi>
2026-03-05 15:08:22 +0200

    Fix

--- scripts/cutthrough_timeseries.py
+++ scripts/cutthrough_timeseries.py
@@ -42,7 +42,7 @@ def memory_usage_psutil():
     import psutil
 
     process = psutil.Process(os.getpid())
-    mem = process.get_memory_info()[0] / float(2**20)
+    mem = process.memory_info()[0] / float(2**20)
     return mem
 
 
