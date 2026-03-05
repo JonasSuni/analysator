4247a22a27b04bfecb783ab0d0f347ead8b44dca
JonasSuni <jonas.suni@helsinki.fi>
2026-03-05 15:29:16 +0200

    Revert to leaking state

--- scripts/cutthrough_timeseries.py
+++ scripts/cutthrough_timeseries.py
@@ -108,8 +108,8 @@ def jplots(
             interpolation_order=interpolation_order,
         )
         data_arr[idx, :] = linecut[2]
-        if fnr in fnr_arr[::10]:  # Garbage collect every 10 steps
-            gc.collect()
+        # if fnr in fnr_arr[::10]:  # Garbage collect every 10 steps
+        #     gc.collect()
         print("fnr = {}, Using {} MB of memory".format(fnr, memory_usage_psutil()))
     if filt > 0:
         data_arr = data_arr - uniform_filter1d(data_arr, size=filt, axis=0)
